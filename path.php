41fc783723f2a2eb529da826f11f8a2bf5d68a13
kozinak228 <kozinak228@github.com>
2026-03-09 20:39:28 +0700

    fix(path): dynamically detect htp/https protocol to avoid SSL image block

--- path.php
+++ path.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('SITE_ROOT')) {
     define('SITE_ROOT', __DIR__);
-    $is_local = in_array($_SERVER['HTTP_HOST'] ?? '', ['localhost', 'localhost:8000', '127.0.0.1']);
-    define('BASE_URL', $is_local ? "http://localhost:8000/" : "https://avtotachka.ru/");
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    define('BASE_URL', $protocol . $_SERVER['HTTP_HOST'] . "/");
     define('ROOT_PATH', realpath(dirname(__FILE__)));
 }
